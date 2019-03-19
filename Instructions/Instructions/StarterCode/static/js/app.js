044a1a5c814eaf7f718e32d1a558cefb0b341d66
Sean Lane <slane078@gmail.com>
2019-03-18 19:16:48 -0700

    homework

---
+++ Instructions/Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,30 @@
+// from data.js
+let tableData = data;
+
+let tbody = d3.select("tbody");
+
+function table(data) {
+  tbody.html("");
+    
+  data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      let ufoInfo = row.append("td");
+        ufoInfo.text(val);
+      }
+    );
+  });
+}
+
+function buttonClick() {
+
+ d3.event.preventDefault();
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+  if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+  table(filteredData);
+}
+d3.selectAll("#filter-btn").on("click", buttonClick);
+table(tableData);
